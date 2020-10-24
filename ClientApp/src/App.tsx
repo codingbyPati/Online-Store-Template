c964bb07cfc52cd28e5ba97211bc5de5186c969c
Pati <patrusia19@gmail.com>
2020-10-24 13:39:52 +0100

    Test commit

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
+import Background from './components/Background';
 
 export default () => (
-    <div>
-
-    </div>
+    <Background/>
 );
---
+++ ClientApp/src/components/Background.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import '../css/Background.css';
+
+export default class Background extends React.Component {
+    public render() {
+        return (
+            <React.Fragment>
+                <div className="page-background">
+
+                    </div>
+            </React.Fragment>
+        )
+    }
+}
